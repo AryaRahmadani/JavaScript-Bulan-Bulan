b58e5fa7563b9d2c8c832aca8ac7fc8661555c66
AryaRahmadani <140323992+AryaRahmadani@users.noreply.github.com>
2023-09-01 10:13:30 +0700

    Add files via upload

---
+++ Java Script/javascript.js
@@ -0,0 +1,127 @@
+let a=3;
+let b=2;
+let d=4;
+let e=5;
+let c=((b+a)*d)/((b*a*a)-e)+b;
+console.log(c);
+
+// let x=6;
+// let y=5;
+// if (x> y){
+//     console.log("benar");
+// }else{
+//     console.log("salah");
+// }
+
+// let  nilaia=90;
+// let  nilaib=70;
+// let  nilaic=60;
+// let  max= 70;
+// if (nilaia,nilaib,nilaic>max) {
+//     console.log("Anda Lulus");
+// }else {
+//     console.log("Anda Tidak Lulus");
+// }
+
+let bulan =12;
+let tanggal=31;
+let Bulan="Belum di buat";
+
+if(bulan==1){
+    if(tanggal>0 && tanggal<20){
+        bulan="jawir";
+    }
+    if(tanggal>19 && tanggal<31){
+        bulan="Ustad";
+    }
+}
+if(bulan==2){
+    if(tanggal>0 && tanggal<15){
+        bulan="gen cina";
+    }
+    if(tanggal>15 && tanggal<30){
+        bulan="Uzumaki";
+    }
+}
+if(bulan==3){
+    if(tanggal>0 && tanggal<19){
+        bulan="Uciha"
+    }
+    if(tanggal>19 && tanggal<30){
+        bulan="Katrina"
+    }
+}
+if(bulan==4){
+    if(tanggal>0 && tanggal<13){
+        bulan="Akatsuki";
+    }
+    if(tanggal>13 && tanggal<30){
+        bulan="Member Suki";
+    }
+}
+if(bulan==5){
+    if(tanggal>0 && tanggal<18){
+        bulan="Ectomorph";
+    }
+    if(tanggal>18 && tanggal<30){
+        bulan="Oktomorph";
+    }
+}
+if(bulan==6){
+    if(tanggal>0 && tanggal<11){
+        bulan="Gita";
+    }
+    if(tanggal>11 && tanggal<31){
+        bulan="Azizi";
+    }
+}
+if(bulan==7){
+    if(tanggal>0 && tanggal<17){
+        bulan="Zee";
+    }
+    if(tanggal>17 && tanggal<30){
+        bulan="Shania";
+    }
+}
+if(bulan==8){
+    if(tanggal>0 && tanggal>18){
+        bulan="Jesslyn";
+    }
+    if(tanggal>18   && tanggal<30){
+        bulan="Freya";
+    }
+}
+if(bulan==9){
+    if(tanggal>0 && tanggal<12){
+        bulan="Gabriel";
+    }
+    if(tanggal>12 && tanggal<31){
+        bulan="raja ";
+    }
+}
+if(bulan==10){
+    if(tanggal>0 && tanggal<13){
+        bulan="alat";
+    }
+    if(tanggal>13 && tanggal<30){
+        bulan="Raja Iblis";
+    }
+}
+if(bulan==11){
+    if(tanggal>0 && tanggal<14){
+        bulan="Kroco";
+    }
+    if(tanggal>14 && tanggal<31){
+        bulan="setboy";
+    }
+}
+if(bulan==12){
+    if(tanggal>0 && tanggal<15){
+        bulan="batbuy";
+    }
+    if(tanggal>15 && tanggal<30){
+        bulan="batman";
+    }
+}
+console.log(bulan);
+
